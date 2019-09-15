9cb40426d24e3435ec5134e6a3cda45541a49b50
Anne Geiersbach <annasazi@gmail.com>
2019-09-15 09:14:49 -0400

    Validate glaze recipe ingredient amount between 0 and 100

--- app/models/glaze_recipe_ingredient.rb
+++ app/models/glaze_recipe_ingredient.rb
@@ -1,4 +1,6 @@
 class GlazeRecipeIngredient < ApplicationRecord
   belongs_to :glaze_material
   belongs_to :glaze_recipe_version
+
+  validates :amount, numericality: { less_than_or_equal_to: 100, greater_than_or_equal_to: 0 }
 end
--- test/models/glaze_recipe_ingredient_test.rb
+++ test/models/glaze_recipe_ingredient_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class GlazeRecipeIngredientTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid model" do
+    gri = GlazeRecipeIngredient.new(
+      amount: 20,
+      glaze_recipe_version: glaze_recipe_versions(:one),
+      glaze_material: glaze_materials(:one)
+    )
+    assert gri.valid?
+  end
+
+  test "validates numericality of amount" do
+    gri = glaze_recipe_ingredients(:one)
+    gri.amount = 101
+
+    refute gri.valid?
+    assert gri.errors[:amount].include?("must be less than or equal to 100")
+
+    gri.amount = -20
+    refute gri.valid?
+    assert gri.errors[:amount].include?("must be greater than or equal to 0")
+
+    gri.amount = 0
+    assert gri.valid?
+  end
 end
